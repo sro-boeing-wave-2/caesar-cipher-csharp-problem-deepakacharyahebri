5a65259ef3515e8f3e8aa3b333a8c1df3fd9e14a
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-18 21:08:46 +0530

    cipher text solution has been updated

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,21 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string output = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                int AsciiValue = (int)text[i];
+                if (AsciiValue >= 65 && AsciiValue <= 90)
+                {
+                    output += (char)(((int)text[i] + shiftKey - 65) % 26 + 65);
+                }
+                else if (AsciiValue >= 97 && AsciiValue <= 122)
+                {
+                    output += (char)(((int)text[i] + shiftKey - 97) % 26 + 97);
+                }
+                else output += text[i];
+            }
+            return output;
         }
     }
 }
